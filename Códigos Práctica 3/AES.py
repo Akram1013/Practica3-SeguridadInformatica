8757b4b943ca1d37ac3b2054ea14bde27e54cba6
Akram <80918031+Akram1013@users.noreply.github.com>
2023-04-16 18:03:17 +0200

    Update AES.py

--- Códigos Práctica 3/AES.py
+++ Códigos Práctica 3/AES.py
@@ -1,18 +1,19 @@
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
-def encrypt(key, data):
-    cipher = AES.new(key, AES.MODE_CBC, key)
-    ciphertext = cipher.encrypt(data)
+def encrypt(frase, aes):
+    data = frase.encode("utf-8")
+    data = pad(data, 16)
+    ciphertext = aes.encrypt(data)
     return ciphertext.hex()
 
-def decrypt(key, data):
-    decipher = AES.new(key, AES.MODE_CBC, key)
+def decrypt(frase, aes):
+    data = bytes.fromhex(frase)
     
     if len(data)%16 != 0:
         data = pad(data, 16)
     
-    deciphertext = decipher.decrypt(data)
+    deciphertext = aes.decrypt(data)
     return unpad(deciphertext, 16).decode("ascii")
 
 
@@ -22,15 +23,13 @@ print("Introduzca la clave:")
 clave = input()
 
 key = clave.encode("utf-8")
+aes = AES.new(key, AES.MODE_CBC, key)
 
 print("1) Encriptar\n2) Desencriptar")
 mode = int(input())
 if mode == 1:
-    data = frase.encode("utf-8")
-    data = pad(data, 16)
-    print(encrypt(key, data))
+    print(encrypt(frase, aes))
 elif mode == 2:
-    data = bytes.fromhex(frase)
-    print(decrypt(key, data))
+    print(decrypt(frase, aes))
 else:
     print("El modo no es correcto")
